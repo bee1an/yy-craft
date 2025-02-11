9169112bd88b6c39b9bd6539cac8b02dfb2c1eaa
lichanglin <beelan@dingtalk.net>
2025-02-11 16:41:53 +0800

    Add rollup-plugin-dts package

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import yyUi from '@yy-ui/yy-ui'
+import yyUi from '../../dist'
 import plugin from './plugins'
 import router from './router'
-// 全局引入样式
-// import '@yy-ui/theme-chalk/src/index.scss'
 
 const app = createApp(App)
 app.use(yyUi).use(plugin).use(router)
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,16 +3,24 @@ import esbuild from 'rollup-plugin-esbuild'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import resolve from '@rollup/plugin-node-resolve'
+// import { dts } from 'rollup-plugin-dts'
 
-export default defineConfig({
-  input: 'packages/components/virtual-list/index.ts', // TODO: 测试打包依赖
-  output: {
-    format: 'esm',
-    // dir: 'dist',
-    sourcemap: false,
-    file: 'dist/index.js'
-  },
-  external: ['vue'],
-  // jsx: 'preserve',
-  plugins: [vue(), vueJsx(), esbuild(), resolve({ extensions: ['.ts'] })]
-})
+export default defineConfig([
+  {
+    input: 'packages/yy-ui/index.ts', // TODO: 测试打包依赖
+    output: {
+      format: 'esm',
+      // dir: 'dist',
+      sourcemap: false,
+      file: 'dist/index.js'
+    },
+    external: ['vue'],
+    // jsx: 'preserve',
+    plugins: [vue(), vueJsx(), esbuild(), resolve({ extensions: ['.ts'] })]
+  }
+  // {
+  //   input: 'packages/yy-ui/index.ts',
+  //   output: [{ file: 'dist/types.d.ts', format: 'esm' }],
+  //   plugins: [dts()]
+  // }
+])

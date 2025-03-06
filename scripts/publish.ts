d4c1373a70dbb4b4ffb14288f7dc401033f7e945
lichanglin <beelan@yeah.net>
2025-03-06 10:26:46 +0800

    chore: update note.md

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -2,17 +2,12 @@ import shelljs from 'shelljs' // 命令执行器
 import path from 'node:path'
 import { rootDir } from './utils/paths'
 
-// 升级版本号
-// const currentVersion = version
-// const versionArr = currentVersion.split('.')
-// versionArr.push(parseInt(versionArr.pop()) + 1) // 版本号加1
-// let newVersion = versionArr.join('.')
-
 async function publish() {
   shelljs.exec('pnpm i --frozen-lockfile') // 该命令强制基于现有 lockfile 安装依赖（不更新 lockfile），用于确保依赖树绝对一致
   shelljs.exec('pnpm build')
 
-  // @ts-expect-error 打包后会有这个路径
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const { default: version } = await import('../dist/yy-ui/es/version.js')
 
   const pkgFile = path.resolve(rootDir, './dist/yy-ui/package.json')

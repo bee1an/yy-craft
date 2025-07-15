94c3a52157232cfeebe6c2c67ad75f3e6d66d4d6
bee1an <beelan@yeah.net>
2025-07-15 16:01:05 +0800

    chore(config-provider): :hammer: update

--- packages/components/config-provider/src/config-provider.tsx
+++ packages/components/config-provider/src/config-provider.tsx
@@ -1,33 +1,34 @@
-import { providerKey } from '@yy-ui/constants/src/providerKey'
+import type { ThemeKey } from "@yy-ui/theme-chalk";
+import { providerKey } from "@yy-ui/constants/src/providerKey";
 import {
 	type ExtractPropTypes,
 	type PropType,
 	computed,
 	defineComponent,
 	provide,
-	reactive
-} from 'vue'
+	reactive,
+} from "vue";
 
 export const configProviderProps = {
 	theme: {
-		type: String as PropType<'light' | 'dark' | (string & {})>,
-		default: 'light'
-	}
-}
+		type: String as PropType<ThemeKey | (string & {})>,
+		default: "light",
+	},
+};
 
-export type ConfigProviderProps = ExtractPropTypes<typeof configProviderProps>
+export type ConfigProviderProps = ExtractPropTypes<typeof configProviderProps>;
 
 export default defineComponent({
-	name: 'ConfigProvider',
+	name: "ConfigProvider",
 	props: configProviderProps,
 	setup(props) {
 		const provideValue = reactive({
-			theme: computed(() => props.theme)
-		})
+			theme: computed(() => props.theme),
+		});
 
-		provide(providerKey, provideValue)
+		provide(providerKey, provideValue);
 	},
 	render() {
-		return this.$slots.default?.()
-	}
-})
+		return this.$slots.default?.();
+	},
+});

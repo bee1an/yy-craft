755cd679cd90e52a796be766ca8fa3671829e235
bee1an <beelan@yeah.net>
2025-07-15 16:01:55 +0800

    chore: :hammer: change inject key

--- packages/theme-chalk/src/common/index.ts
+++ packages/theme-chalk/src/common/index.ts
@@ -1,6 +1,5 @@
-export { default as commonDark } from './dark'
-export { default as commonLight } from './light'
-export { default as commonBase } from './_common'
+export { default as commonDark } from "./dark";
+export { default as commonLight } from "./light";
+export { default as commonBase } from "./_common";
 
-export * from './config'
-export * from './type'
+export * from "./type";

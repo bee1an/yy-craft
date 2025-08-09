e972783767230532bae166941433b4aa3250a74a
bee1an <beelan@yeah.net>
2025-08-09 20:00:23 +0800

    fix: :bug: change fault variable name

--- packages/theme-chalk/src/slider/_common.ts
+++ packages/theme-chalk/src/slider/_common.ts
@@ -3,7 +3,7 @@ import { commonBase } from '../common'
 export default {
   height: '6px',
   borderRadius: '3px',
-  trggerWidth: '18px',
+  triggerWidth: '18px',
   triggerHeight: '18px',
   transitionDuration: commonBase.transitionDuration,
   transitionTimingFunction: commonBase.cubicBezierEaseInOut,
